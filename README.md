8cac5f101055ed2232176aa689e598d658fd8132
Abishake Srithar <sritharabishake@gmail.com>
2025-05-12 07:53:11 +0100

    Install and apply Prettier Code Formatter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fitness-tracker-frontend
+
 Fitness Tracker Frontend
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import './style.css'
-import { getAllEntries } from './get/all-entries'
-import { getAllExercises } from './get/all-exercises'
-import { getAllWorkouts } from './get/all-workouts'
+import "./style.css";
+import { getAllEntries } from "./get/all-entries";
+import { getAllExercises } from "./get/all-exercises";
+import { getAllWorkouts } from "./get/all-workouts";
 
-
-
-document.getElementById("getAllEntriesButton")!.onclick = getAllEntries
-document.getElementById("getAllExercisesButton")!.onclick = getAllExercises
-document.getElementById("getAllWorkoutsButton")!.onclick = getAllWorkouts
+document.getElementById("getAllEntriesButton")!.onclick = getAllEntries;
+document.getElementById("getAllExercisesButton")!.onclick = getAllExercises;
+document.getElementById("getAllWorkoutsButton")!.onclick = getAllWorkouts;
