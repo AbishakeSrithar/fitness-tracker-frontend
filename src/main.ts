747a299f18fafbb488421e4182365a69c2afd0eb
Abishake Srithar <sritharabishake@gmail.com>
2025-05-21 09:17:14 +0100

    Install ChartJS and some dependencies, can see a graph

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,38 @@ import { addEventListenerForDeleteExercise } from "./core/exercise/delete/delete
 import { addEventListenerForDeleteEntry } from "./core/entry/delete/deleteEntry";
 import { addEventListenerForDeleteWorkout } from "./core/workout/delete/deleteWorkout";
 
+//ChartJS
+import Chart, { type ChartItem } from 'chart.js/auto'
+
+(async function() {
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+
+  new Chart(
+    document.getElementById('acquisitions') as ChartItem,
+    {
+      type: 'bar',
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+})();
+
+
 // Get All
 document.getElementById("getAllEntriesButton")!.onclick = getAllEntries;
 document.getElementById("getAllExercisesButton")!.onclick = getAllExercises;
@@ -61,3 +93,5 @@ addEventListenerForUpdateWorkout();
 addEventListenerForDeleteExercise();
 addEventListenerForDeleteEntry();
 addEventListenerForDeleteWorkout();
+
+
